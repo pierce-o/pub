1c86ec3f2c69330644ad186dc42a77d2b6dc912f
Pierce O'Sullivan <9221336+pierce-o@users.noreply.github.com>
2018-10-30 22:56:11 +0000

    Add more for when a device is inserted.
    Added -
      - Getting the drive letter.
      - Getting volume information.
      - Accessing and creating the registry.
      - Looping through all the devices and checking if the current device is in that list.
    
    Todo -
      - Have it so that the user can select to whitelist devices.
      - Have the user define where the backup location is.
      - Have the user specify what level of backup they want.

--- pub/Form1.cs
+++ pub/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.Collections;
 using System.Runtime.InteropServices;
 using System.Diagnostics;
+using Microsoft.Win32; // Used for the registery
 
 namespace pub
 {
@@ -20,10 +21,37 @@ namespace pub
         public Int32 dbch_size;
         public Int32 dbch_devicetype;
         public Int32 dbch_reserved;
+        public Int32 dbcv_unitmask;
+    }
+
+    public class volumeInformation
+    {
+
+        public volumeInformation()
+        {
+            volumeName = new StringBuilder(261);
+            serialNumber = -1337;
+            maxComponentLen = -1337;
+            fileSysetmFlags = -1337;
+            fileSystemName = new StringBuilder(261);
+        }
+        
+
+        public StringBuilder volumeName;
+        public Int32 serialNumber;
+        public Int32 maxComponentLen;
+        public Int32 fileSysetmFlags;
+        public StringBuilder fileSystemName;
+
     }
 
     public partial class Form1 : Form
     {
+
+        /* WinAPI imports*/
+
+        [DllImport("kernel32.dll")]
+        static extern bool GetVolumeInformation( string lpRootPathName, StringBuilder lpVolumeNameBuffer, Int32 nVolumeNameSize, out Int32 lpVolumeSerialNumber, out Int32 lpMaximumComponentLength, out Int32 lpFileSystemFlags, StringBuilder lpFileSystemNameBuffer, Int32 nFileSystemNameSize );
 
         /* Required WNDPROC  */
 
@@ -40,6 +68,26 @@ namespace pub
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public string convertToDriveLetter(int mask)
+        {
+
+            int i; // Uninitialized at the moment as it gets set at the start of the for loop.
+
+            for ( i = 0; i < 26; i++ ) // Loop for all the possible drive letters which is 26 
+            {
+
+                if ( (mask & 0x1) == 1 ) // Check if the mask is currently 1 which means that the letter is at this offset.
+                    break; // Exit the loop.
+                else
+                    mask = mask >> 1; // Shift the mask over one to check on the next loop.
+
+            }
+
+            // A is the base offset of the char since the first capital letter of the ascii table then each letter after that is one up each time.
+            return ( (char)( Convert.ToChar( 'A' + i ) ) ).ToString(); 
+
         }
 
         protected override void WndProc(ref Message m)
@@ -65,8 +113,49 @@ namespace pub
                             // Now that we have idenified that it is a volume type we need to get the specifc
                             // information about the drive.
 
-                            
-                            
+                            string driveLetter = convertToDriveLetter(hdr.dbcv_unitmask) + ":"; // ":" is needed for the GetVolumeInformationA function.
+
+                            volumeInformation volumeInfo = new volumeInformation();
+
+                            if(GetVolumeInformation(driveLetter, volumeInfo.volumeName, volumeInfo.volumeName.Capacity, out volumeInfo.serialNumber, out volumeInfo.maxComponentLen, out volumeInfo.fileSysetmFlags, volumeInfo.fileSystemName, volumeInfo.fileSystemName.Capacity ))
+                            {
+
+                                RegistryKey registryKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\pub\devices");
+
+                                if(registryKey == null) // Create that sub key
+                                    Registry.CurrentUser.CreateSubKey(@"SOFTWARE\pub\devices");
+                                else
+                                {
+                                    // Proceed to check if the device has been added to the pool
+
+                                    string[] devices = registryKey.GetSubKeyNames();
+
+                                    foreach( string device in devices )
+                                    {
+
+                                        RegistryKey rk = registryKey.OpenSubKey( device, false );
+
+                                        if (rk != null)
+                                        {
+                                            if ((string)rk.GetValue("serialnumber") == volumeInfo.serialNumber.ToString("X8"))
+                                            {
+                                                Debug.WriteLine("Serial Number found.");
+                                                break;
+                                            }
+                                        }
+
+                                        rk.Close();
+
+                                    }
+
+                                    registryKey.Close();
+                                }
+
+                            }
+            
+                            Debug.WriteLine(volumeInfo.serialNumber.ToString("X8"));
+
+                           
 
                         }
 
